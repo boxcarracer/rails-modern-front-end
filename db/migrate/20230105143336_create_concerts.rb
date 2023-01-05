dffdb274eb13601fca503910b1a0e000bb7f0337
boxcarracerone <redsockssssss@gmail.com>
2023-01-05 19:39:49 +0400

    Add concert enums to postgres

--- db/migrate/20230105143336_create_concerts.rb
+++ db/migrate/20230105143336_create_concerts.rb
@@ -1,5 +1,8 @@
 class CreateConcerts < ActiveRecord::Migration[7.0]
   def change
+    create_enum :enum_ilk, %w[concert meet_n_greet battle]
+    create_enum :enum_access, %w[general members vips]
+
     create_table :concerts do |t|
       t.string :name
       t.text :description
