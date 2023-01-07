233bf5a3b952a14e9afe16d6f7f003c81eb97fe4
boxcarracerone <redsockssssss@gmail.com>
2023-01-07 19:11:10 +0400

    Use turboframes for favorites

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
+    './app/views/**/*.turbostream.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
-    './config/initializers/simple_form_tailwind.rb'
+    './config/initializers/simple_form_tailwind.rb',
   ]
 }
