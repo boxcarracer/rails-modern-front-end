a8cf5648bafd5c5f1226d2b51347d2c1937aeee3
boxcarracerone <redsockssssss@gmail.com>
2023-01-07 18:15:28 +0400

    Move favorites to turbo frame

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
+  def index; end
+
   def create
     Favorite.create(user: current_user, concert_id: params[:concert_id])
-    redirect_to(:root)
+    render partial: "favorites/list"
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
-    redirect_to(:root)
+    render partial: "favorites/list"
   end
 
   private def favorite_params
